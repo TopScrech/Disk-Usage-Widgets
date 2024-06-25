32d8dd35fb6f07d6ac51b40d2de1ac5943ffec1f
TopScrech <sergei_saliukov@icloud.com>
2024-06-25 10:42:17 +0200

    Initial Commit

---
+++ Disk Usage Widgets/Disk_Usage_WidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Disk_Usage_WidgetsApp.swift
+//  Disk Usage Widgets
+//
+//  Created by Sergei Saliukov on 25/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Disk_Usage_WidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
