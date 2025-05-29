bc7535c6b5250d75df765fede6c103302d858f94
TopScrech <sergei_saliukov@icloud.com>
2025-05-29 13:12:34 +0200

    Toolbar is a separate view + added help description to it

---
+++ Disk Usage Widgets/Home View Toolbar.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+import WidgetKit
+
+struct HomeViewToolbar: View {
+    @Environment(VM.self) private var vm
+    
+    var body: some View {
+        Button {
+            refresh()
+        } label: {
+            Image(systemName: "arrow.trianglehead.2.clockwise.rotate.90")
+                .bold()
+        }
+        .keyboardShortcut("r")
+        .help("⌘ + R to refresh")
+    }
+    
+    private func refresh() {
+        vm.listAvailableDisks()
+        WidgetCenter.shared.reloadAllTimelines()
+    }
+}
+
+#Preview {
+    HomeViewToolbar()
+        .environment(VM())
+}
