967c70aecba7de7f8f9652bd33abf59c8f5166d7
TopScrech <89252798+TopScrech@users.noreply.github.com>
2026-07-27 23:46:41 +0200

    Added iOS target

---
+++ iOS/iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp.swift
+//  iOS
+//
+//  Created by Sergei Saliukov on 27/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
